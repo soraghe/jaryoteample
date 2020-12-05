10daf82d5ef9041474d81e5ddf1ce5d167ab36cb
KKing <50602824+kingtaewoo@users.noreply.github.com>
2020-12-05 17:03:17 +0900

    Update bankadministrator.cpp
    1. 로그인에 따른 관리자 권한 부여
    2. 관리자 로그아웃과 프로그램 종료 구분
    3. 버그 수정(데이터 초기화 관련)

--- administrator/bankadministrator.cpp
+++ administrator/bankadministrator.cpp
@@ -15,7 +15,7 @@ void signalHandler(int signum);
 int main(int argc, char const *argv[]) {
 
 	MsgAdmin admin;
-	key_t msq_key = ftok(".", 31);
+	key_t msq_key = ftok("/", 31);
 	if(msq_key == -1){
 		perror("ftok() error!(ipc 키생성 실패) : ");
 		kill(getpid(), SIGINT);
@@ -29,15 +29,17 @@ int main(int argc, char const *argv[]) {
 
 	//시그널 제어방식 재정의
 	signal(SIGINT, signalHandler);
-	
+	memset(&admin, 0x00, sizeof(MsgAdmin));
+
 	AdminInfo admin_localInfo;
 	memset(&admin_localInfo, 0x00, sizeof(AdminInfo));
 
+	bool admin_login = false;
 	while(1)
 	{
 		//메시지 타입 구조체 초기화
-		memset(&admin, 0x00, sizeof(MsgAdmin));
-		cout << "===작업 선택===\n1.관리자 로그인\n2.클라이언트 정보 조회\n3.클라이언트 정보수정\n4.관리자 등록\n5.관리자 로그아웃\n";
+
+		cout << "===작업 선택===\n1.관리자 로그인\n2.클라이언트 정보 조회\n3.클라이언트 정보수정\n4.관리자 등록\n5.관리자 로그아웃\n6.프로그램 종료\n";
 		cout << ">>>>";
 		cin >> admin.cmd;
 		switch(admin.cmd)
@@ -76,198 +78,213 @@ int main(int argc, char const *argv[]) {
 						//로그인 승인 결과 수신 성공 시 결과에 따라 메시지 출력
 						else if(rcvSize != -1) {
 							if(admin.is_error == true){
-								cout << "로그인 실패." << endl;
+								cout << "로그인 실패.\n" << endl;
+								//로그인 실패시 초기화
+								memset(&admin, 0x00, sizeof(MsgAdmin));
 								break;
 							}
 							else{
+								admin_login = true;
 								cout << "관리자 " << admin.adminId << "님 안녕하세요.\n" << endl;
 							}
 						}
 
 						break;
 					}
-			case ADSEARCHCLIENT : {       //클라이언트 조회
-						       int num;  // 선택할 변수
-						       char admin_data_id[20] = {0};
-						       char admin_data_name[20] = {0};
-						       char admin_data_account[20] = {0};
+			case ADSEARCHCLIENT : {       
+						      if(admin_login == true){
+							      //클라이언트 조회
+							      int num;  // 선택할 변수
+							      char admin_data_id[20] = {0};
+							      char admin_data_name[20] = {0};
+							      char admin_data_account[20] = {0};
 
-						       cout << "\n--- 클라이언트 정보 조회 ---\n1.ID로 찾기\n2.이름으로 찾기\n3.계좌번호로 찾기" << endl;
-						       //admin에 수정할 정보를 담아 메시지 송신
-						       cin >> num;
-						       switch(num){
-							       case 1 : {
-										cout << "검색할 ID 입력" <<endl << ">>>";
-										cin >> admin_data_id;
-										strcpy(admin.data.clientId , admin_data_id);
-										break;	
-									}
-							       case 2 : {
-										cout << "검색할 이름 입력" <<endl << ">>>";
-										cin >> admin_data_name;
-										strcpy(admin.data.clientName , admin_data_name);
-										break;
-									}
-							       case 3 : {
-										cout << "검색할 계좌번호 입력" << endl << ">>>";
-										cin >> admin_data_account;
-										strcpy(admin.data.clientAccountNum , admin_data_account);
-										break;
-									}
-									break;
-						       }	
-						        strcpy(admin.adminId, admin_localInfo.adminId);
-						       admin.mtype = MSG_TYPE_ADMIN;
-						       int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
-						       //msgsnd() 예외처리
-						       if(sndSize != 0){
-							       perror("msgsnd() error!(고객정보 조회 요청 실패) ");
-							       kill(getpid(), SIGINT);
-						       }
+							      cout << "\n--- 클라이언트 정보 조회 ---\n1.ID로 찾기\n2.이름으로 찾기\n3.계좌번호로 찾기" << endl;
+							      //admin에 수정할 정보를 담아 메시지 송신
+							      cin >> num;
+							      switch(num){
+								      case 1 : {
+										       cout << "검색할 ID 입력" <<endl << ">>>";
+										       cin >> admin_data_id;
+										       strcpy(admin.data.clientId , admin_data_id);
+										       break;	
+									       }
+								      case 2 : {
+										       cout << "검색할 이름 입력" <<endl << ">>>";
+										       cin >> admin_data_name;
+										       strcpy(admin.data.clientName , admin_data_name);
+										       break;
+									       }
+								      case 3 : {
+										       cout << "검색할 계좌번호 입력" << endl << ">>>";
+										       cin >> admin_data_account;
+										       strcpy(admin.data.clientAccountNum , admin_data_account);
+										       break;
+									       }
+									       break;
+							      }	
+							      strcpy(admin.adminId, admin_localInfo.adminId);
+							      admin.mtype = MSG_TYPE_ADMIN;
+							      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
+							      //msgsnd() 예외처리
+							      if(sndSize != 0){
+								      perror("msgsnd() error!(고객정보 조회 요청 실패) \n");
+								      kill(getpid(), SIGINT);
+							      }
 
-						       int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
-						       //msgrcv() 에외처리
-						       if(rcvSize == -1) {
-							       perror("msgrcv() error!(고객 정보 수신 실패) ");
-							       kill(getpid(), SIGINT);
-						       }
-						       //서버에서 보내는 정보 수수신 성공 시 결과에 따라 메시지 출력
-						       else if(rcvSize != -1) {
-							       if(admin.is_error == true){
-								       cout << "고객정보 조회 실패." << endl;
-								       break;
-							       }
-							       else{
-								       cout << "\n검색된  고객정보" << endl;
-								       cout << "고객 ID : " <<  admin.data.clientId << endl;
-								       cout << "고객 비밀번호 : " << admin.data.clientPw << endl;
-								       cout << "고객 이름 : " << admin.data.clientName << endl;
-								       cout << "고객 주민등록 번호 : " << admin.data.clientResRegNum << endl;	
-								       cout << "고객 계좌번호 : " << admin.data.clientAccountNum << endl;	
-								       cout << "고객 계좌 잔액 : " <<admin.data.clientBalance << endl << endl;	           
-							       }
-						       }
-						       break;
+							      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
+							      //msgrcv() 에외처리
+							      if(rcvSize == -1) {
+								      perror("msgrcv() error!(고객 정보 수신 실패)\n ");
+								      kill(getpid(), SIGINT);
+							      }
+							      //서버에서 보내는 정보 수수신 성공 시 결과에 따라 메시지 출력
+							      else if(rcvSize != -1) {
+								      if(admin.is_error == true){
+									      cout << "고객정보 조회 실패." << endl;
+									      break;
+								      }
+								      else{
+									      cout << "\n검색된  고객정보" << endl;
+									      cout << "고객 ID : " <<  admin.data.clientId << endl;
+									      cout << "고객 비밀번호 : " << admin.data.clientPw << endl;
+									      cout << "고객 이름 : " << admin.data.clientName << endl;
+									      cout << "고객 주민등록 번호 : " << admin.data.clientResRegNum << endl;	
+									      cout << "고객 계좌번호 : " << admin.data.clientAccountNum << endl;	
+									      cout << "고객 계좌 잔액 : " <<admin.data.clientBalance << endl << endl;	           
+								      }
+							      }
 
-					       }
+						      }
+						      else {
+							      cout << "관리자 권한 없음, 로그인 해주세요\n" << endl;
+						      }
+						      break;
+
+					      }
 			case ADMODIFYCLINFO : {
-						      int num;
-						      char admin_data_id[20] = {0};
-						      char admin_data_pw[20] = {0};
-						      char admin_data_name[20] = {0};
-						      char admin_data_account[20] = {0};
+						      if(admin_login == true){
+							      int num;
+							      char admin_data_id[20] = {0};
+							      char admin_data_pw[20] = {0};
+							      char admin_data_name[20] = {0};
+							      char admin_data_account[20] = {0};
 
-						      //클라이언트 정보 수정
-						      cout << "\n--- 클라이언트 정보 수정 ---" << endl;
-						      cout << "수정할 고객의 ID  입력\n>>> ";
-						      cin >> admin_data_id;
-							  
-						     //>>여기에 잘못된 ID입력에 대한 처리를 해주세요(server/bankserver.cpp 참고)
+							      //클라이언트 정보 수정
+							      cout << "\n--- 클라이언트 정보 수정 ---" << endl;
+							      cout << "수정할 고객의 ID  입력\n>>> ";
+							      cin >> admin_data_id;
+
+							      //>>여기에 잘못된 ID입력에 대한 처리를 해주세요(server/bankserver.cpp 참고)
 
 
-						      cout << "수정할 고객의 데이터 선택\n1.비밀번호\n2.고객 이름\n3.고객 계좌번호\n>>> ";
-						      cin >> num;
-						      switch(num)
-						      {
-							      case 1: {
-									      cout << "수정할 비밀번호 입력\n>>> ";
-									      cin >> admin_data_pw;
-									      cout.clear();cin.clear();
+							      cout << "수정할 고객의 데이터 선택\n1.비밀번호\n2.고객 이름\n3.고객 계좌번호\n>>> ";
+							      cin >> num;
+							      switch(num)
+							      {
+								      case 1: {
+										      cout << "수정할 비밀번호 입력\n>>> ";
+										      cin >> admin_data_pw;
+										      cout.clear();cin.clear();
 
-									      //admin에 수정할 정보를 담아 메시지 송신
-									      admin.mtype = MSG_TYPE_ADMIN;
-									      strcpy(admin.data.clientId , admin_data_id);
-									      strcpy(admin.data.clientPw, admin_data_pw);
-									      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
-									      //msgsnd() 예외처리
-									      if(sndSize != 0) {
-										      perror("msgsnd() error!(로그인 - ID/PW 전송 실패) ");
-										      kill(getpid(), SIGINT);
-									      }
-									      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
-									      //msgrcv() 에외처리
-									      if(rcvSize == -1) {
-										      perror("msgrcv() error!(고객 정보 수정 수신  실패) ");
-										      kill(getpid(), SIGINT);
-									      }
-									      //고객정보 수정 수신 성공 시 결과에 따라 메시지 출력
-									      else if(rcvSize != -1) {
-										      if(admin.is_error == true){
-											      cout << "고객정보 수정 실패(사유 : 해당 ID없음)\n" << endl;
-											      break;
+										      //admin에 수정할 정보를 담아 메시지 송신
+										      admin.mtype = MSG_TYPE_ADMIN;
+										      strcpy(admin.data.clientId , admin_data_id);
+										      strcpy(admin.data.clientPw, admin_data_pw);
+										      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
+										      //msgsnd() 예외처리
+										      if(sndSize != 0) {
+											      perror("msgsnd() error!(로그인 - ID/PW 전송 실패) ");
+											      kill(getpid(), SIGINT);
 										      }
-										      else{
-											      cout << "고객정보 수정 완료.\n" << endl;
+										      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
+										      //msgrcv() 에외처리
+										      if(rcvSize == -1) {
+											      perror("msgrcv() error!(고객 정보 수정 수신  실패) ");
+											      kill(getpid(), SIGINT);
 										      }
+										      //고객정보 수정 수신 성공 시 결과에 따라 메시지 출력
+										      else if(rcvSize != -1) {
+											      if(admin.is_error == true){
+												      cout << "고객정보 수정 실패(사유 : 해당 ID없음)\n" << endl;
+												      break;
+											      }
+											      else{
+												      cout << "고객정보 수정 완료.\n" << endl;
+											      }
+										      }
+										      break;
 									      }
-									      break;
-								      }
-							      case 2: {
-									      cout << "수정할 고객 이름 입력\n>>> ";
-									      cin >> admin_data_name;
-									      cout.clear();cin.clear();
+								      case 2: {
+										      cout << "수정할 고객 이름 입력\n>>> ";
+										      cin >> admin_data_name;
+										      cout.clear();cin.clear();
 
-									      //admin에 수정할 정보를 담아 메시지 송신
-									      admin.mtype = MSG_TYPE_ADMIN;
-									      strcpy(admin.data.clientId , admin_data_id);
-									      strcpy(admin.data.clientName, admin_data_name);
-									      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
-									      //msgsnd() 예외처리
-									      if(sndSize != 0){
-										      perror("msgsnd() error!(로그인 - ID/PW 전송 실패) ");
-										      kill(getpid(), SIGINT);
-									      }
-									      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
-									      //msgrcv() 에외처리
-									      if(rcvSize == -1) {
-										      perror("msgrcv() error!(고객 정보 수정 수신  실패) ");
-										      kill(getpid(), SIGINT);
-									      }
-									      //고객정보 수정 수신 성공 시 결과에 따라 메시지 출력
-									      else if(rcvSize != -1) {
-										      if(admin.is_error == true){
-											      cout << "고객정보 수정 실패(사유 : 해당 ID없음)\n" << endl;
-											      break;
+										      //admin에 수정할 정보를 담아 메시지 송신
+										      admin.mtype = MSG_TYPE_ADMIN;
+										      strcpy(admin.data.clientId , admin_data_id);
+										      strcpy(admin.data.clientName, admin_data_name);
+										      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
+										      //msgsnd() 예외처리
+										      if(sndSize != 0){
+											      perror("msgsnd() error!(로그인 - ID/PW 전송 실패) ");
+											      kill(getpid(), SIGINT);
 										      }
-										      else{
-											      cout << "고객정보 수정 완료.\n" << endl;
+										      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
+										      //msgrcv() 에외처리
+										      if(rcvSize == -1) {
+											      perror("msgrcv() error!(고객 정보 수정 수신  실패) ");
+											      kill(getpid(), SIGINT);
 										      }
+										      //고객정보 수정 수신 성공 시 결과에 따라 메시지 출력
+										      else if(rcvSize != -1) {
+											      if(admin.is_error == true){
+												      cout << "고객정보 수정 실패(사유 : 해당 ID없음)\n" << endl;
+												      break;
+											      }
+											      else{
+												      cout << "고객정보 수정 완료.\n" << endl;
+											      }
+										      }
+										      break;
 									      }
-									      break;
-								      }
-							      case 3: {
-									      cout << "수정할 고객 계좌번호 입력\n>>> ";
-									      cin >> admin_data_account;
-									      cout.clear();cin.clear();
+								      case 3: {
+										      cout << "수정할 고객 계좌번호 입력\n>>> ";
+										      cin >> admin_data_account;
+										      cout.clear();cin.clear();
 
-									      //admin에 수정할 정보를 담아 메시지 송신
-									      admin.mtype = MSG_TYPE_ADMIN;
-									      strcpy(admin.data.clientId , admin_data_id);
-									      strcpy(admin.data.clientAccountNum, admin_data_account);
-									      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
-									      //msgsnd() 예외처리
-									      if(sndSize != 0){
-										      perror("msgsnd() error!(로그인 - ID/PW 전송 실패) ");
-										      kill(getpid(), SIGINT);
-									      }
-									      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
-									      //msgrcv() 에외처리
-									      if(rcvSize == -1) {
-										      perror("msgrcv() error!(고객 정보 수정 수신  실패) ");
-										      kill(getpid(), SIGINT);
-									      }
-									      //고객정보 수정 수신 성공 시 결과에 따라 메시지 출력
-									      else if(rcvSize != -1) {
-										      if(admin.is_error == true){
-											      cout << "고객정보 수정 실패(사유 : 해당 ID없음)\n" << endl;
-											      break;
+										      //admin에 수정할 정보를 담아 메시지 송신
+										      admin.mtype = MSG_TYPE_ADMIN;
+										      strcpy(admin.data.clientId , admin_data_id);
+										      strcpy(admin.data.clientAccountNum, admin_data_account);
+										      int sndSize = msgsnd(msq_id, &admin, MSG_SIZE_ADMIN, 0);
+										      //msgsnd() 예외처리
+										      if(sndSize != 0){
+											      perror("msgsnd() error!(로그인 - ID/PW 전송 실패) ");
+											      kill(getpid(), SIGINT);
 										      }
-										      else{
-											      cout << "고객정보 수정 완료.\n" << endl;
+										      int rcvSize = msgrcv(msq_id, &admin, MSG_SIZE_ADMIN, MSG_TYPE_ADMIN, 0);
+										      //msgrcv() 에외처리
+										      if(rcvSize == -1) {
+											      perror("msgrcv() error!(고객 정보 수정 수신  실패) ");
+											      kill(getpid(), SIGINT);
 										      }
+										      //고객정보 수정 수신 성공 시 결과에 따라 메시지 출력
+										      else if(rcvSize != -1) {
+											      if(admin.is_error == true){
+												      cout << "고객정보 수정 실패(사유 : 해당 ID없음)\n" << endl;
+												      break;
+											      }
+											      else{
+												      cout << "고객정보 수정 완료.\n" << endl;
+											      }
+										      }
+										      break;
 									      }
-									      break;
-								      }
+							      }
+						      } else {
+
+							      cout << "관리자 권한 없음. 로그인 해주세요.\n"<< endl;
 						      }
 						      break;
 					      }	      
@@ -275,7 +292,7 @@ int main(int argc, char const *argv[]) {
 						char admin_id[20] = {0}; 
 						char admin_pw[20] = {0};
 
-						while(1) {
+						if (admin_login==true) {
 							//사용자 입력
 							cout << "\n----- 관리자 등록 -----" << endl;
 							cout << "관리자 ID : ";
@@ -307,20 +324,31 @@ int main(int argc, char const *argv[]) {
 							else if(rcvSize != -1) {
 								if(admin.is_error == true){
 									cout << "관리자 정보 등록 실패." << endl;
+									memset(&admin, 0x00, sizeof(MsgAdmin));
 									break;
 								}
 								else{
 									cout << "관리자 " << admin.adminId << " 등록 완료.\n" << endl;
 								}
 							}
-						break;
+							break;
+						} else 
+						{
+							cout << "관리자 권한 없음. 로그인 해주세요\n" << endl;
+
 						}
 						break;
 					}
 			case 5 : {
-					 cout << "\n관리자 로그아웃." << endl;
-					 kill(getpid(), SIGINT);
+
+					 cout << "관리자 로그아웃.\n" << endl;
+					 admin_login = false;
+					 break;
 				 }
+			case 6: {
+					cout <<"프로그램 종료.\n" << endl;
+					kill(getpid(), SIGINT);
+				}
 			default: {      //예상치 못한 오류
 					 cout << "잘못된 입력. 다시 입력하십시오." << endl;
 					 break;
